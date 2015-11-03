d8c85bc66d10a2bbe16a4396447ec9cef8834bf9
automotua <adam.junqiang.li@gmail.com>
2015-11-03 15:52:19 -0500

    added process_xxpkt into the main flow

--- peer.c
+++ peer.c
@@ -8,31 +8,16 @@
  * Skeleton for 15-441 Project 2.
  *
  * ==================================================
- * 
+ *
  * Modified by: Ke Wu <kewu@andrew.cmu.edu>
- *  
+ *
  * Date: 10/23/2015
  *
- * Description: Skeleton for 15-441 Project 2, receive input from user or 
- *              receive packets from peers, then process them. 
+ * Description: Skeleton for 15-441 Project 2, receive input from user or
+ *              receive packets from peers, then process them.
  *
  */
-
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include "debug.h"
-#include "spiffy.h"
-#include "bt_parse.h"
-#include "input_buffer.h"
-#include "whohas_ihave.h"
-#include "pkt_helper.h"
-#include "helper.h"
+#include "peer.h"
 
 #define SELECT_TIMEOUT 5
 
@@ -64,7 +49,7 @@ int main(int argc, char **argv) {
 
   /* ignore SIGPIPE in case server is terminated due to broken pipe */
   signal(SIGPIPE, SIG_IGN);
-  
+
   /* main routine */
   peer_run(&config);
   return 0;
@@ -79,14 +64,14 @@ void process_inbound_udp(int sock, bt_config_t *config) {
 
   /* receive UDP packet */
   fromlen = sizeof(from);
-  int ret = spiffy_recvfrom(sock, buf, BUFLEN, 0, 
+  int ret = spiffy_recvfrom(sock, buf, BUFLEN, 0,
                             (struct sockaddr *) &from, &fromlen);
-  
-  DPRINTF(DEBUG_SOCKETS, "Incoming message from %s:%d\n%s\n\n", 
+
+  DPRINTF(DEBUG_SOCKETS, "Incoming message from %s:%d\n%s\n\n",
      inet_ntoa(from.sin_addr),
      ntohs(from.sin_port),
      buf);
-  
+
 
   /* Demultiplex */
   int type = demultiplexing(ret, buf);
@@ -100,13 +85,17 @@ void process_inbound_udp(int sock, bt_config_t *config) {
         process_Ihave_packet(ret, buf, config, &from);
         break;
     case 2:
+        process_getpkt(ret, buf, config, &from);
         break;
     case 3:
+        process_data_packet(buf, config, &from);
         break;
     case 4:
+        process_ack_packet(buf, config, &from);
         break;
     case 5:
-        break; 
+        process_deniedpkt(ret, buf, config, &from);
+        break;
   }
 
 }
@@ -120,7 +109,7 @@ void process_get(bt_config_t* config) {
         memcpy(hashs[i], config->get_chunks.chunks[i].hash, CHUNK_HASH_SIZE);
     }
 
-    /* generate WHOHAS packets, maybe more than 1 packet due to max length 
+    /* generate WHOHAS packets, maybe more than 1 packet due to max length
      * limit of a UDP packet */
     int max_packet_len, last_packet_len, packets_size;
     char** whohas_packet = generate_whohas(config->get_chunks.size,
@@ -143,10 +132,10 @@ void process_get(bt_config_t* config) {
           continue;
         }
         for (int i = 0; i < packets_size; i++){
-            int packet_len = 
+            int packet_len =
                     (i == packets_size-1) ? last_packet_len : max_packet_len;
 
-            send_packet(config->sock, whohas_packet[i], packet_len, 0, 
+            send_packet(config->sock, whohas_packet[i], packet_len, 0,
                         (struct sockaddr *)&peer->addr, sizeof(peer->addr));
         }
         peer = peer->next;
@@ -173,12 +162,12 @@ void handle_user_input(char *line, void *cbdata) {
         strcpy(config->output_file, outf);
         /* write chunks that I already owned to output file */
         for (int i = 0; i < config->get_chunks.size; i++){
-          int index = find_chunk(&conig->has_chunks, 
+          int index = find_chunk(&conig->has_chunks,
                                   config->get_chunks.chunks[i].hash);
           if (index != -1){
-            char* data = read_chunk_data_from_file(config, 
+            char* data = read_chunk_data_from_file(config,
                                           config->get_chunks.chunks[i].hash);
-            write_chunk_data_to_file(config, data, 512*1024, 
+            write_chunk_data_to_file(config, data, 512*1024,
                                     config->get_chunks.chunks[i].id * 512*1024);
             config->written_chunks[i] = 1;
             free(data);
@@ -186,7 +175,7 @@ void handle_user_input(char *line, void *cbdata) {
         }
         /* flood WHOHAS packet */
         process_get(config);
-        
+
     }
   }
 }
@@ -197,31 +186,31 @@ void peer_run(bt_config_t *config) {
   struct sockaddr_in myaddr;
   fd_set readyset;
   struct user_iobuf *userbuf;
-  
+
   if ((userbuf = create_userbuf()) == NULL) {
     perror("peer_run could not allocate userbuf");
     exit(-1);
   }
-  
+
   if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP)) == -1) {
     perror("peer_run could not create socket");
     exit(-1);
   }
 
   config->sock = sock;
-  
+
   bzero(&myaddr, sizeof(myaddr));
   myaddr.sin_family = AF_INET;
   myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
   myaddr.sin_port = htons(config->myport);
-  
+
   if (bind(sock, (struct sockaddr *) &myaddr, sizeof(myaddr)) == -1) {
     perror("peer_run could not bind socket");
     exit(-1);
   }
-  
+
   spiffy_init(config->identity, (struct sockaddr *)&myaddr, sizeof(myaddr));
-  
+
   struct timeval timeoutSelect = {SELECT_TIMEOUT, 0};
 
   /* init readset */
@@ -231,23 +220,23 @@ void peer_run(bt_config_t *config) {
   config->max_fd = sock;
   while (1) {
     int nfds;
-    readyset = config->readset;    
+    readyset = config->readset;
     nfds = select(config->max_fd+1, &readyset, NULL, NULL, &timeoutSelect);
-    
+
     if (nfds > 0) {
       /* a packet comes from Internet */
       if (FD_ISSET(sock, &readyset)) {
           process_inbound_udp(sock, config);
           nfds--;
       }
-    
+
       /* get input from stdin */
       if (FD_ISSET(STDIN_FILENO, &readyset)) {
           process_user_input(STDIN_FILENO, userbuf, handle_user_input,
              (void*)config);
           nfds--;
       }
-      
+
       /* some connection maybe timeout */
       bt_peer_t peer = config->peers;
       while (nfds > 0 && peer) {
